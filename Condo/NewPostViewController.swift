7b1419e5ed55f1f71ba6244ad7034a531dbbfbc7
rjos <RodolfoSoares@Rodolfo-Jose.local>
2015-06-11 18:04:48 -0300

    Add delegates para TextField, TextView e Picker View

--- Condo/NewPostViewController.swift
+++ Condo/NewPostViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class NewPostViewController: UIViewController {
+class NewPostViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UITextViewDelegate{
 
     @IBOutlet weak var typeUser: UITextField!
+    @IBOutlet weak var countLetter: UILabel!
+    @IBOutlet weak var textReport: UITextView!
+    @IBOutlet weak var btnPublish: UIButton!
+    
+    var pickerTypeUser : UIPickerView!
+    var toolBar : UIToolbar!
+    var imgProfile : UIImageView!
+    
+    var typesUserArray = ["Síndico", "Público"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.btnPublish.enabled = false
+        
+        self.pickerTypeUser = UIPickerView()
+        self.pickerTypeUser.delegate = self
+        
+        self.typeUser.inputView = pickerTypeUser
+        
+        self.toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 44))
+        self.toolBar.barStyle = UIBarStyle.Default
+        
+        let btnDone : UIBarButtonItem! = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done, target: self, action: Selector("SelectedItemPickerView"))
+        let btnCancel : UIBarButtonItem! = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel, target: self, action: Selector("CancelItemPickerView"))
+        
+        self.toolBar.setItems([btnCancel, UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil) , btnDone], animated: true)
+        
+        self.typeUser.inputAccessoryView = self.toolBar
+        
+        self.imgProfile = UIImageView(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        self.imgProfile.layer.cornerRadius = self.imgProfile.bounds.width / 2
+        self.imgProfile.clipsToBounds = true
+        self.imgProfile.image = UIImage(named: "dummy-photo-adm")
+        
+        var bezierPath : UIBezierPath = UIBezierPath(rect: CGRect(x: 10, y: 0, width: self.imgProfile.bounds.width, height: self.imgProfile.bounds.height))
+        
+        self.textReport.textContainer.exclusionPaths = [bezierPath]
+        self.textReport.addSubview(self.imgProfile)
+        
+        self.textReport.layer.borderWidth = 1.0
+        self.textReport.layer.borderColor = UIColor.blueColor().CGColor
         // Do any additional setup after loading the view.
     }
 
@@ -23,11 +61,45 @@ class NewPostViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return false
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.typesUserArray.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return self.typesUserArray[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.typeUser.text = self.typesUserArray[row]
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.typeUser.resignFirstResponder()
+        self.textReport.resignFirstResponder()
+    }
+    
     @IBAction func ShowBack(sender: AnyObject) {
     }
 
     @IBAction func ShowPublish(sender: AnyObject) {
     }
+    
+    func SelectedItemPickerView(){
+        self.typeUser.resignFirstResponder()
+    }
+    
+    func CancelItemPickerView(){
+        self.typeUser.resignFirstResponder()
+    }
+    
     /*
     // MARK: - Navigation
 
