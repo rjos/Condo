84847dc7a99ccd92fc45caca2facccd2ccf36ae0
lucasvtenorio <Lucasvtenorio@gmail.com>
2015-06-15 15:55:42 -0300

    CondoApiMapper

---
+++ CondoModel/CondoModel/CondoApiMapper.swift
@@ -0,0 +1,20 @@
+//
+//  CondoApiMapper.swift
+//  CondoModel
+//
+//  Created by Lucas Tenório on 15/06/15.
+//  Copyright (c) 2015 Condo. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class CondoApiMapper: NSObject {
+    static func communityFromPFObject(object: PFObject) -> Community? {
+        return nil
+    }
+    
+    static func expenseFromPFOBject(object: PFObject) -> Expense? {
+        return nil
+    }
+}
