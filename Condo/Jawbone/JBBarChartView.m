df1f76a70ed6cc9f2768bd27ab6d412bc2f30aca
lucasvtenorio <Lucasvtenorio@gmail.com>
2015-06-16 12:08:42 -0300

    Revert "Testes de import do Parse"
    This reverts commit b93b8e6c48e09675142c955a7c3615e9eb164ea4.
    
    Revertendo uma besteira que eu cometi

--- Condo/Jawbone/JBBarChartView.m
+++ Condo/Jawbone/JBBarChartView.m
@@ -397,10 +397,7 @@ static UIColor *kJBBarChartViewDefaultBarColor = nil;
             NSUInteger index = 0;
             for (UIView *barView in self.barViews)
             {
-#pragma mark - ______HACK_PARA_MELHORAR_A_ANIMACAO_________
-                //NSTimeInterval oldDelay = (kJBBarChartViewStateAnimationDuration * 0.5) * index;
-                
-                [UIView animateWithDuration:kJBBarChartViewStateAnimationDuration delay:0.0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+                [UIView animateWithDuration:kJBBarChartViewStateAnimationDuration delay:(kJBBarChartViewStateAnimationDuration * 0.5) * index options:UIViewAnimationOptionBeginFromCurrentState animations:^{
                     updateBarView(barView, YES);
                 } completion:^(BOOL finished) {
                     [UIView animateWithDuration:kJBBarChartViewStateAnimationDuration delay:0.0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
